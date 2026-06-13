f07aa5612ee6724b784219589fa100b7cb78e575
Chandan01097 <cb131778@gmail.com>
2026-06-13 23:20:37 +0530

    today learning

--- src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
+++ src/main/java/com/codingshuttle/youtube/hospitalManagement/config/AppConfig.java
@@ -3,6 +3,12 @@ package com.codingshuttle.youtube.hospitalManagement.config;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class AppConfig {
@@ -11,4 +17,25 @@ public class AppConfig {
     public ModelMapper modelMapper() {
         return new ModelMapper();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+ }
+  //  @Bean
+    UserDetailsService userDetailsService (){
+
+        UserDetails user1 = User.withUsername("Admin")
+                .password(passwordEncoder().encode("pass"))
+                .roles("ADMIN")
+                .build();
+
+        UserDetails user2 = User.withUsername("Doctor")
+                .password(passwordEncoder().encode("pas-doc"))
+                .roles("DOCTOR")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1,user2);
+    }
+
 }
